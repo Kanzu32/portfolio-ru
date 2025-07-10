9100dae52c9a58070f67a296fe0a485b36669200
Kanzu <55020589+Kanzu32@users.noreply.github.com>
2025-07-10 18:43:13 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -43,7 +43,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     // Component.Graph(),
     Component.SocialLinks(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    // Component.DesktopOnly(Component.TableOfContents()),
     // Component.Backlinks(),
   ],
 }
