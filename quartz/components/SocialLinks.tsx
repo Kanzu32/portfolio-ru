c04943a75c59e3cfb4c19a8a4d541979a6c151fc
Kanzu <55020589+Kanzu32@users.noreply.github.com>
2025-07-11 16:15:33 +0300

    Update SocialLinks.tsx

--- quartz/components/SocialLinks.tsx
+++ quartz/components/SocialLinks.tsx
@@ -6,19 +6,19 @@ export default (() => {
       <div class="social-links">
         <ul>
             <li>
-                <a class="external" href="https://kanzu32.github.io/portfolio-en">🔠 English version</a>
+                <a class="external" href="https://kanzu32.github.io/portfolio-en" target="_blank">🔠 English version</a>
             </li>
 
             <br/>
 
             <li>
-              <a class="external" href="https://github.com/Kanzu32">🛠️ GitHub</a>
+              <a class="external" href="https://github.com/Kanzu32" target="_blank">🛠️ GitHub</a>
             </li>
             <li>
-              <a class="external" href="https://kanzu32.itch.io/">🕹️ Itch.io</a>
+              <a class="external" href="https://kanzu32.itch.io/" target="_blank">🕹️ Itch.io</a>
             </li>
             <li>
-              <a class="external" href="https://t.me/Kanzu32">📟 Telegram</a>
+              <a class="external" href="https://t.me/Kanzu32" target="_blank">📟 Telegram</a>
             </li>
         </ul>
       </div>
